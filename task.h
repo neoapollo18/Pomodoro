c37b72bf34fb4cc1268ae5d2717f78532c5973f0
achan391-debug <achan391@usc.edu>
2025-12-05 14:05:57 -0800

    Update task.h

--- task.h
+++ task.h
@@ -1,1 +1,20 @@
+#ifndef TASK_H
+#define TASK_H
 
+#include <string>
+
+class Task {
+private:
+    std::string name;
+    int completedWorkSessions;
+
+public:
+    Task(const std::string& taskName);
+
+    std::string getName() const;
+    int getCompletedWorkSessions() const;
+
+    void incrementCompleted();
+};
+
+#endif
