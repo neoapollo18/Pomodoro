e96f9d6c4e0f0f68e57721997496f307c8acf7a9
achan391-debug <achan391@usc.edu>
2025-12-05 14:06:29 -0800

    Update task.cpp

--- task.cpp
+++ task.cpp
@@ -1,1 +1,21 @@
+#include "task.h"
+using namespace std;
+
+Task::Task(const std::string& taskName)
+{
+}
+
+std::string Task::getName() const
+{
+    return "";
+}
+
+int Task::getCompletedWorkSessions() const
+{
+    return 0;
+}
+
+void Task::incrementCompleted()
+{
+}
 
